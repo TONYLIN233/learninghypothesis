0f5c7718f6bd67c8d1f612254acabd1346237469
TONYLIN233 <99322416+TONYLIN233@users.noreply.github.com>
2025-09-19 09:45:12 +0800

    feat:首次探索

---
+++ makeintegers.py
@@ -0,0 +1,13 @@
+from hypothesis import given, strategies as st, note
+import pytest
+
+@given(st.integers(min_value=1, max_value=100))
+def test_integers_in_range(n):
+    """
+    1.min_value=1, max_value=100依照这个范围随机取数执行100次，
+    2. 使用note作为执行自动化用例失败的时候调试用
+    3.使用控制台命令执行用例 pytest makeintegers.py -s
+    """
+    note(f"n is: {n}")  # 特别记录 n 的值，失败时显示
+    print(f"打印出整数值{n}")
+    assert 1 <= n <= 100
